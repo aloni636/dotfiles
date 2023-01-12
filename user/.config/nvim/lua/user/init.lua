e59f03b9b4040605a95151c6226f46d1fe60b783
Elwex123 <elyasaf123456789@gmail.com>
2023-01-12 13:30:01 +0200

    Added leap-spooky to nvim config

--- user/.config/nvim/lua/user/init.lua
+++ user/.config/nvim/lua/user/init.lua
@@ -381,6 +381,23 @@ local config = {
       ["ggandor/leap.nvim"] = {
         config = function() require("leap").add_default_mappings() end,
       },
+      ["ggandor/leap-spooky.nvim"] = { config = function()
+        require("leap-spooky").setup {
+          affixes = {
+            -- These will generate mappings for all native text objects, like:
+            -- (ir|ar|iR|aR|im|am|iM|aM){obj}.
+            -- Special line objects will also be added, by repeating the affixes.
+            -- E.g. `yrr<leap>` and `ymm<leap>` will yank a line in the current
+            -- window.
+            -- You can also use 'rest' & 'move' as mnemonics.
+            remote = { window = "r", cross_window = "R" },
+            magnetic = { window = "m", cross_window = "M" },
+          },
+
+          -- at the cursor position if the unnamed register is in use.
+          paste_on_remote_yank = false,
+        }
+      end },
       ["smjonas/live-command.nvim"] = {
         config = function()
           require("live-command").setup {
